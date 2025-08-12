fedc771326d4978a7d3167fa009a509b06a2e168
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-08-12 17:12:44 +0200

    Run high-deps tests on PHP 8.4

--- Tests/PropertyAccessorTest.php
+++ Tests/PropertyAccessorTest.php
@@ -1009,20 +1009,24 @@ class PropertyAccessorTest extends TestCase
 
     private function createUninitializedObjectPropertyGhost(): UninitializedObjectProperty
     {
-        if (!class_exists(ProxyHelper::class)) {
-            $this->markTestSkipped(\sprintf('Class "%s" is required to run this test.', ProxyHelper::class));
+        if (\PHP_VERSION_ID < 80400) {
+            if (!class_exists(ProxyHelper::class)) {
+                $this->markTestSkipped(\sprintf('Class "%s" is required to run this test.', ProxyHelper::class));
+            }
+
+            $class = 'UninitializedObjectPropertyGhost';
+
+            if (!class_exists($class)) {
+                eval('class '.$class.ProxyHelper::generateLazyGhost(new \ReflectionClass(UninitializedObjectProperty::class)));
+            }
+
+            $this->assertTrue(class_exists($class));
+
+            return $class::createLazyGhost(initializer: function ($instance) {
+            });
         }
 
-        $class = 'UninitializedObjectPropertyGhost';
-
-        if (!class_exists($class)) {
-            eval('class '.$class.ProxyHelper::generateLazyGhost(new \ReflectionClass(UninitializedObjectProperty::class)));
-        }
-
-        $this->assertTrue(class_exists($class));
-
-        return $class::createLazyGhost(initializer: function ($instance) {
-        });
+        return (new \ReflectionClass(UninitializedObjectProperty::class))->newLazyGhost(fn () => null);
     }
 
     /**
